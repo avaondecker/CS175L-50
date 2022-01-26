8eeed628a01a77c66f24e26d7713f43831ae483a
avaondecker <91210480+avaondecker@users.noreply.github.com>
2022-01-26 09:21:57 -0500

    myInfo.py

---
+++ myInfo.py
@@ -0,0 +1,8 @@
+#CS175L-50
+#Ava Ondecker
+#myInfo
+
+print("My name is Ava Ondecker")
+print("My address is 87 Ravine Ave, West Caldwell, New Jersey, 07006")
+print("My telephone number is 973-980-1422")
+print("My college major is software engineering")
