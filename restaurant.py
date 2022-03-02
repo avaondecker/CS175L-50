d02c574ce22e022c8c1ecb33ffb71e6088e7168b
avaondecker <91210480+avaondecker@users.noreply.github.com>
2022-03-02 18:27:45 -0500

    restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -2,58 +2,62 @@
 #Ava Ondecker
 #restaurant
 
-vegetarian = input("Is anyone in your party a vegetarian? ")
+ongoing = 'yes'
 
-if vegetarian == "yes" or vegetarian == "no":
-    
-    vegan = input("Is anyone in your party a vegan? ")
-    
+while ongoing == "yes":
 
-    if vegan == "yes" or vegan == "no":
+    vegetarian = input("\nIs anyone in your party a vegetarian? ")
+
+    if vegetarian == "yes" or vegetarian == "no":
         
-        glutenFree = input("Is anyone in your party gluten-free? ")
+        vegan = input("Is anyone in your party a vegan? ")
         
 
-        if glutenFree == "yes" or glutenFree == "no":
-            print("\nHere are your restaurant choices:\n")
+        if vegan == "yes" or vegan == "no":
+            
+            glutenFree = input("Is anyone in your party gluten-free? ")
+            
 
-            if vegetarian == "yes":
-                
-                if vegan == "yes":
+            if glutenFree == "yes" or glutenFree == "no":
+                print("\nHere are your restaurant choices:\n")
 
-                    if glutenFree == "yes" or glutenFree == "no":
-                        print("Corner Cafe\n" + \
-                              "The Chef's Kitchen\n")
-                else:
+                if vegetarian == "yes":
                     
-                    if glutenFree == "yes":
-                        print("Main Street Pizza Company\n" + \
-                              "Corner Cafe\n" + \
-                              "The Chef's Kitchen\n")
+                    if vegan == "yes":
+
+                        if glutenFree == "yes" or glutenFree == "no":
+                            print("Corner Cafe\n" + \
+                                  "The Chef's Kitchen\n")
                     else:
-                        print("Main Street Pizza Company\n" + \
-                              "Corner Cafe\n" + \
-                              "Mama's Fine Italian\n" + \
-                              "The Chef's Kitchen\n")
-            else: # vegetarian == "no"
+                        
+                        if glutenFree == "yes":
+                            print("Main Street Pizza Company\n" + \
+                                  "Corner Cafe\n" + \
+                                  "The Chef's Kitchen\n")
+                        else:
+                            print("Main Street Pizza Company\n" + \
+                                  "Corner Cafe\n" + \
+                                  "Mama's Fine Italian\n" + \
+                                  "The Chef's Kitchen\n")
+                else: # vegetarian == "no"
 
-                if vegan == "yes":
+                    if vegan == "yes":
 
-                    if glutenFree == "yes" or gluten_free == "no":
-                        print("Corner Cafe\nThe Chef's Kitchen\n")
+                        if glutenFree == "yes" or gluten_free == "no":
+                            print("Corner Cafe\nThe Chef's Kitchen\n")
 
-                else: # vegan == "no"
+                    else: # vegan == "no"
 
-                    if glutenFree == "yes":
-                      print("Main Street Pizza Company\n" + \
-                            "Corner Cafe\n" + \
-                            "The Chef's Kitchen\n")
-                                   
-                    else: # glutenFree == "no"
-                        print("Joe's Gourmet Burgers\n" + \
-                              "Main Street Pizza Company\n" + \
-                              "Corner Cafe\n" + \
-                              "Mama's Fine Italian\n" + \
-                              "The Chef's Kitchen\n")
-
-        
+                        if glutenFree == "yes":
+                          print("Main Street Pizza Company\n" + \
+                                "Corner Cafe\n" + \
+                                "The Chef's Kitchen\n")
+                                       
+                        else: # glutenFree == "no"
+                            print("Joe's Gourmet Burgers\n" + \
+                                  "Main Street Pizza Company\n" + \
+                                  "Corner Cafe\n" + \
+                                  "Mama's Fine Italian\n" + \
+                                  "The Chef's Kitchen\n")
+                            
+    ongoing = input("Would you like to do another search? ")
